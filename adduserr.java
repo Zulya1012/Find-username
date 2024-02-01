ecd1635c1b4f6e82adfdf962cb798575ddced7d4
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2024-02-01 19:36:57 +0400

    Add files via upload

---
+++ adduserr.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class adduserr{
+	public static void main(String[] arg){
+		int count=0;
+		String [] username={"Book", "Copybook", "Pen", "Notebook"};
+		Scanner s=new Scanner(System.in);
+		String newname=s.nextLine();
+		for (int i=0; i<username.length; i++){
+			if (username[i].equals(newname)){
+				count=1;
+				System.out.println("This username is in the array");
+				break;
+			}
+		if (count==0){
+			System.out.println("This username isn't in the array");
+			break;
+		}
+		
+		}
+		
+	}	
+}
